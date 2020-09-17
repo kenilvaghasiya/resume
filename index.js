0b50b5c56df7af00f3e362d203d4bc86985ea39b
Kenil Vaghasiya <66935525+kenilvaghasiya@users.noreply.github.com>
2020-09-17 12:03:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,480 @@
+
+var data = ` <div class="resume" id="new" >
+<div class="resume_left">
+  <div class="resume_profile" >
+    <img src="profile.png" alt="profile_pic" id='image'>
+  </div>
+  <div class="resume_content">
+    <div class="resume_item resume_info">
+      <div class="title">
+        <p class="bold" id="name">stephen colbert</p>
+        <p class="regular" id="post">Designer</p>
+      </div>
+      <ul>
+        <li>
+          <div class="icon">
+            <i class="fas fa-map-signs"></i>
+          </div>
+          <div class="data" id='address'>
+            21 Street, Texas <br /> USA
+          </div>
+        </li>
+        <li>
+          <div class="icon">
+            <i class="fas fa-mobile-alt"></i>
+          </div>
+          <div class="data" id="mobile">
+            +324 4445678
+          </div>
+        </li>
+        <li>
+          <div class="icon">
+            <i class="fas fa-envelope"></i>
+          </div>
+          <div class="data" id="email">
+            stephen@gmail.com
+          </div>
+        </li>
+        <li>
+          <div class="icon">
+            <i class="fab fa-weebly"></i>
+          </div>
+          <div class="data" id="website">
+            www.stephen.com
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="resume_item resume_skills">
+      <div class="title">
+        <p class="bold">skill's</p>
+      </div>
+      <ul>
+        <li>
+          <div class="skill_name" id="sk1">
+            HTML
+          </div>
+          <div class="skill_progress">
+            <span style="width: 80%;" id="skp1"></span>
+          </div>
+          <div class="skill_per" id="skpp1">80%</div>
+        </li>
+        <li>
+          <div class="skill_name"  id="sk2">
+            CSS
+          </div>
+          <div class="skill_progress">
+            <span style="width: 70%;" id="skp2"></span>
+          </div>
+          <div class="skill_per" id="skpp2">70%</div>
+        </li>
+        <li>
+          <div class="skill_name"  id="sk3">
+            SASS
+          </div>
+          <div class="skill_progress">
+            <span style="width: 90%;" id="skp3"></span>
+          </div>
+          <div class="skill_per" id="skpp3">90%</div>
+        </li>
+        <li>
+          <div class="skill_name"  id="sk4">
+            JS
+          </div>
+          <div class="skill_progress">
+            <span style="width: 60%;" id="skp4"></span>
+          </div>
+          <div class="skill_per" id="skpp4">60%</div>
+        </li>
+        <li>
+          <div class="skill_name" id="sk5">
+            JQUERY
+          </div>
+          <div class="skill_progress">
+            <span style="width: 88%;" id="skp5"></span>
+          </div>
+          <div class="skill_per" id="skpp5">88%</div>
+        </li>
+      </ul>
+    </div>
+    <div class="resume_item resume_social">
+      <div class="title">
+        <p class="bold">Social</p>
+      </div>
+      <ul>
+        <li>
+          <div class="icon">
+            <i class="fab fa-facebook-square"></i>
+          </div>
+          <div class="data">
+            <p class="semi-bold">Facebook</p>
+            <p id="sos1">Stephen@facebook</p>
+          </div>
+        </li>
+        <li>
+          <div class="icon">
+            <i class="fab fa-twitter-square"></i>
+          </div>
+          <div class="data">
+            <p class="semi-bold">Twitter</p>
+            <p id="sos2">Stephen@twitter</p>
+          </div>
+        </li>
+        <li>
+          <div class="icon">
+            <i class="fab fa-youtube"></i>
+          </div>
+          <div class="data">
+            <p class="semi-bold">Youtube</p>
+            <p id="sos3">Stephen@youtube</p>
+          </div>
+        </li>
+        <li>
+          <div class="icon">
+            <i class="fab fa-linkedin"></i>
+          </div>
+          <div class="data">
+            <p class="semi-bold">Linkedin</p>
+            <p id="sos4">Stephen@linkedin</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+<div class="resume_right">
+  <div class="resume_item resume_about">
+      <div class="title">
+        <p class="bold">About us</p>
+      </div id="about">
+      <p id="about">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis illo fugit officiis distinctio culpa officia totam atque exercitationem inventore repudiandae?</p>
+  </div>
+  <div class="resume_item resume_work">
+      <div class="title">
+        <p class="bold">Work Experience</p>
+      </div>
+      <ul>
+          <li>
+              <div class="date">2013 - 2015</div> 
+              <div class="info">
+                  <p class="semi-bold">Lorem ipsum dolor sit amet.</p> 
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, voluptatibus!</p>
+              </div>
+          </li>
+          <li>
+            <div class="date">2015 - 2017</div>
+            <div class="info">
+                  <p class="semi-bold">Lorem ipsum dolor sit amet.</p> 
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, voluptatibus!</p>
+              </div>
+          </li>
+          <li>
+            <div class="date">2017 - Present</div>
+            <div class="info">
+                  <p class="semi-bold">Lorem ipsum dolor sit amet.</p> 
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, voluptatibus!</p>
+              </div>
+          </li>
+      </ul>
+  </div>
+  <div class="resume_item resume_education">
+    <div class="title">
+        <p class="bold">Education</p>
+      </div>
+    <ul>
+          <li>
+              <div class="date">2010 - 2013</div> 
+              <div class="info">
+                  <p class="semi-bold">Web Designing (Texas University)</p> 
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, voluptatibus!</p>
+              </div>
+          </li>
+          <li>
+            <div class="date">2000 - 2010</div>
+            <div class="info">
+                  <p class="semi-bold">Texas International School</p> 
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, voluptatibus!</p>
+              </div>
+          </li>
+      </ul>
+  </div>
+  <div class="resume_item resume_hobby">
+    <div class="title">
+        <p class="bold">Hobby</p>
+      </div>
+    <ul>
+      <li><i class="fas fa-book"></i></li>
+      <li><i class="fas fa-gamepad"></i></li>
+      <li><i class="fas fa-music"></i></li>
+      <li><i class="fab fa-pagelines"></i></li>
+    </ul>
+  </div>
+</div>
+</div>`
+
+try {
+  rightscr = document.getElementById('rightscr').innerHTML = data;
+}
+catch{ }
+
+try {
+  var nameor = document.getElementById('name').innerText;
+  var postor = document.getElementById('post').innerText;
+  var addressor = document.getElementById('address').innerText;
+  var mobileor = document.getElementById('mobile').innerText;
+  var emailor = document.getElementById('email').innerText;
+  var websiteor = document.getElementById('website').innerText;
+  var imageor = document.getElementById('image');
+  var aboutor = document.getElementById('about').innerText;
+  var IMG = document.getElementById('IMG');
+}
+catch{ }
+
+
+
+
+try{
+
+  var sk1 = document.getElementById('sk1').innerText;
+  var sk2 = document.getElementById('sk2').innerText;
+  var sk3 = document.getElementById('sk3').innerText;
+  var sk4 = document.getElementById('sk4').innerText;
+  var sk5 = document.getElementById('sk5').innerText;
+
+
+  var skp1 = document.getElementById('skp1').style.width;
+  var skp2 = document.getElementById('skp2').style.width;
+  var skp3 = document.getElementById('skp3').style.width;
+  var skp4 = document.getElementById('skp4').style.width;
+  var skp5 = document.getElementById('skp5').style.width;
+
+
+  var skpp1 = document.getElementById('skpp1').innerText;
+  var skpp2 = document.getElementById('skpp2').innerText;
+  var skpp3 = document.getElementById('skpp3').innerText;
+  var skpp4 = document.getElementById('skpp4').innerText;
+  var skpp5 = document.getElementById('skpp5').innerText;
+
+
+  var sos1 = document.getElementById('sos1').innerText;
+  var sos2 = document.getElementById('sos2').innerText;
+  var sos3 = document.getElementById('sos3').innerText;
+  var sos4 = document.getElementById('sos4').innerText;
+
+}catch{}
+
+
+IMG.addEventListener("change", function () {
+
+  let file = this.files[0];
+  console.log(file)
+  const reader = new FileReader();
+
+  reader.addEventListener("load", function () {
+    imageor.setAttribute("src", this.result)
+  });
+
+  reader.readAsDataURL(file);
+
+});
+
+function name1(asd) {
+  document.getElementById('name').innerText = asd.value;
+  if (asd.value == "") {
+    document.getElementById('name').innerText = nameor;
+
+  }
+}
+
+function post1(asd) {
+  document.getElementById('post').innerText = asd.value;
+  if (asd.value == "") {
+    document.getElementById('post').innerText = postor;
+
+  }
+}
+
+function address1(asd) {
+  document.getElementById('address').innerText = asd.value;
+  if (asd.value == "") {
+    document.getElementById('address').innerText = addressor;
+
+  }
+}
+
+function mobile1(asd) {
+  document.getElementById('mobile').innerText = asd.value;
+  if (asd.value == "") {
+    document.getElementById('mobile').innerText = mobileor;
+
+  }
+}
+function email1(asd) {
+  document.getElementById('email').innerText = asd.value;
+  if (asd.value == "") {
+    document.getElementById('email').innerText = emailor;
+
+  }
+}
+function website1(asd) {
+  document.getElementById('website').innerText = asd.value;
+  if (asd.value == "") {
+    document.getElementById('website').innerText = websiteor;
+
+  }
+}
+function about1(asd) {
+  document.getElementById('about').innerText = asd.value;
+  if (asd.value == "") {
+    document.getElementById('about').innerText = aboutor;
+
+  }
+}
+
+
+function skil1(asd) {
+  document.getElementById('sk1').innerText = asd.value;
+  if (asd.value == "") {
+    document.getElementById('sk1').innerText = sk1;
+
+  }
+}
+
+function skil2(asd) {
+  document.getElementById('sk2').innerText = asd.value;
+  if (asd.value == "") {
+    document.getElementById('sk2').innerText = sk2;
+
+  }
+}
+
+function skil3(asd) {
+  document.getElementById('sk3').innerText = asd.value;
+  if (asd.value == "") {
+    document.getElementById('sk3').innerText = sk3;
+
+  }
+}
+
+function skil4(asd) {
+  document.getElementById('sk4').innerText = asd.value;
+  if (asd.value == "") {
+    document.getElementById('sk4').innerText = sk4;
+
+  }
+}
+
+function skil5(asd) {
+  document.getElementById('sk5').innerText = asd.value;
+  if (asd.value == "") {
+    document.getElementById('sk5').innerText = sk5;
+
+  }
+}
+
+function skilpr1(asd) {
+  document.getElementById('skp1').style.width = asd.value+'%';
+  if (asd.value == "") {
+    document.getElementById('skp1').style.width =skp1;
+  }
+
+  document.getElementById('skpp1').innerText = asd.value+'%';
+  if (asd.value == "") {
+    document.getElementById('skpp1').innerText =skpp1;
+  }
+}
+
+
+function skilpr2(asd) {
+  document.getElementById('skp2').style.width = asd.value+'%';
+  if (asd.value == "") {
+    document.getElementById('skp2').style.width =skp2;
+  }
+
+  document.getElementById('skpp2').innerText = asd.value+'%';
+  if (asd.value == "") {
+    document.getElementById('skpp2').innerText =skpp2;
+  }
+}
+
+function skilpr3(asd) {
+  document.getElementById('skp3').style.width = asd.value+'%';
+  if (asd.value == "") {
+    document.getElementById('skp3').style.width =skp3;
+  }
+
+  document.getElementById('skpp3').innerText = asd.value+'%';
+  if (asd.value == "") {
+    document.getElementById('skpp3').innerText =skpp3;
+  }
+}
+
+
+function skilpr4(asd) {
+  document.getElementById('skp4').style.width = asd.value+'%';
+  if (asd.value == "") {
+    document.getElementById('skp4').style.width =skp4;
+  }
+
+  document.getElementById('skpp4').innerText = asd.value+'%';
+  if (asd.value == "") {
+    document.getElementById('skpp4').innerText =skpp4;
+  }
+}
+
+
+function skilpr5(asd) {
+  document.getElementById('skp5').style.width = asd.value+'%';
+  if (asd.value == "") {
+    document.getElementById('skp5').style.width =skp5;
+  }
+
+  document.getElementById('skpp5').innerText = asd.value+'%';
+  if (asd.value == "") {
+    document.getElementById('skpp5').innerText =skpp5;
+  }
+}
+
+
+
+function social1(asd) {
+  document.getElementById('sos1').innerText = asd.value;
+  if (asd.value == "") {
+    document.getElementById('sos1').innerText = sos1;
+  }
+}
+
+
+function social2(asd) {
+  document.getElementById('sos2').innerText = asd.value;
+  if (asd.value == "") {
+    document.getElementById('sos2').innerText = sos2;
+  }
+}
+
+function social3(asd) {
+  document.getElementById('sos3').innerText = asd.value;
+  if (asd.value == "") {
+    document.getElementById('sos3').innerText = sos3;
+  }
+}
+
+function social4(asd) {
+  document.getElementById('sos4').innerText = asd.value;
+  if (asd.value == "") {
+    document.getElementById('sos4').innerText = sos4;
+  }
+}
+
+
+
+
+
+
+function pdf() {
+
+  var i1 = document.getElementById("new");
+  html2pdf().from(i1).save();
+
+}
+
